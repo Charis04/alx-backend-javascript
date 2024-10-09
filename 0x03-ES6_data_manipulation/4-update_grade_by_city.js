ab84c1e9eec8cafde4fc53ed37af73147c9c9a58
charis <charisadu@gmail.com>
2024-10-09 20:47:53 +0100

    Create a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,6 +3,6 @@ export default function updateStudentGradeByCity(students, city, grades) {
     .filter((student) => student.location === city)
     .map((student) => {
       const studentGrade = grades.find((grade) => grade.studentId === student.id);
-      return { ...student, grade: studentGrade ? studentGrade.grade : NaN };
+      return { ...student, grade: studentGrade ? studentGrade.grade : 'N/A' };
     });
 }
