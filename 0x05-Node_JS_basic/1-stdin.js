aac46ddc250ef02fc75a1a7db482358dd1155895
Charis Adu <charisadu@gmail.com>
2024-12-07 10:46:35 +0000

    proc

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
-console.log('Welcome to ALX, what is your name?');
+process.stdout.write('Welcome to ALX, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
+  if (process.stdin.isTTY) {
+    process.exit();
+  }
 });
 
 process.stdin.on('end', () => {
