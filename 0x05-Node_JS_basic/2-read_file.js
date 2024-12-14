c54303b70967d2a25e00ed69787a11f3ee67f16b
Charis Adu <charisadu@gmail.com>
2024-12-14 18:13:06 +0000

    fix eslint and catch error

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,27 +1,33 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(filePath) {
-  const data = fs.readFileSync(filePath, "utf-8");
-  const lines = data.split("\n").filter((line) => line.trim() !== "");
-  const header = lines.shift();
-  const students = lines.map((line) => line.split(","));
-  const fields = {};
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    lines.shift();
+    const students = lines.map((line) => line.split(','));
+    const fields = {};
+    fields.dump = [];
 
-  console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students: ${students.length}`);
 
-  students.forEach(([firstName, lastName, age, field]) => {
-    if (!fields[field]) {
-      fields[field] = [];
+    students.forEach(([firstName, lastName, age, field]) => {
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+      fields.dump.push(lastName, age);
+    });
+
+    for (const [field, firstnames] of Object.entries(fields)) {
+      console.log(
+        `Number of students in ${field}: ${
+          firstnames.length
+        }. List: ${firstnames.join(', ')}`,
+      );
     }
-    fields[field].push(firstName);
-  });
-
-  for (const field in fields) {
-    console.log(
-      `Number of students in ${field}: ${fields[field].length}. List: ${fields[
-        field
-      ].join(", ")}`
-    );
+  } catch (error) {
+    throw new Error('Cannot load the database!');
   }
 }
 
