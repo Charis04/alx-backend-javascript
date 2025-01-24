bf7efd627e9a0274807d097d826156acd144054d
charis <charisadu@gmail.com>
2025-01-24 23:15:08 +0100

    Change countStudents function declaration from arrow function to regular function

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const app = express();
 
 // Function to read and process the CSV file
-const countStudents = (filePath) => {
+function countStudents(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
