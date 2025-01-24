9509e8e116eeff7e1cc13bbf76e6fb94905638fe
charis <charisadu@gmail.com>
2025-01-24 06:37:12 +0100

    Update express dependency to version 4.21.2 and add basic Express server setup

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+// Create an instance of the Express application
+const app = express();
+
+// Define the root endpoint
+app.get('/', (req, res) => {
+  res.send('Hello ALX!');
+});
+
+// Make the app listen on port 1245
+const port = 1245;
+app.listen(port, () => {
+  console.log('...');
+});
+
+// Export the app
+module.exports = app;
